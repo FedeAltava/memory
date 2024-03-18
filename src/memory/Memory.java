d1612071a64a8a7df670156fd9b50cc1ea704dcb
nocxil <160049420+nocxil@users.noreply.github.com>
2024-03-18 12:53:32 +0100

    Meto las fotos y las dif. vistas

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -19,7 +19,7 @@ public class Memory extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("clone.fxml"));
         
         Scene scene = new Scene(root);
         
